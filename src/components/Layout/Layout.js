a225d0b2b1f9718955c53e7bdd5ee144a2029574
Casperfeng <casp.feng@gmail.com>
2019-02-01 15:30:27 +0100

    added container for burger and build controller

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Aux from '../../hoc/Aux';
 
+//Aux is used to avoid using a div tag
 const layout = (props) => (
+<Aux>
   <div>Toolbar, SideDrawer, Backdrop</div>
   <main>
     {props.children}
   </main>
+</Aux>
 );
 
 export default layout;
