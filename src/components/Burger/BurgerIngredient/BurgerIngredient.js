6434eb8d4e19bc81dddcec7526c8633555595211
Casperfeng <casp.feng@gmail.com>
2019-02-02 13:18:53 +0100

    ingredients added and exported

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -17,8 +17,21 @@ const burgerIngredient = (props) => {
     );
       break;
   case('meat'):
-      ingredient = <div className={classes.Meat}></div>
+      ingredient = <div className={classes.Meat}></div>;
+      break;
+  case('cheese'):
+    ingredient = <div className={classes.Cheese}></div>;
+      break;
+  case('bacon'):
+    ingredient = <div className={classes.Bacon}></div>;
+      break;
+  case('salad'):
+     ingredient = <div className={classes.Salad}></div>;
+    break;
+  default:
+      ingredient: null;
   }
+  return ingredient;
 };
 
 export default burgerIngredient;
