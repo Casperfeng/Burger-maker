129be05455fb8232eb18db446cdda933d6551239
Casperfeng <casp.feng@gmail.com>
2019-02-03 21:43:13 +0100

    build control visuals now finished

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 
 class BurgerBuilder extends Component {
@@ -8,17 +9,17 @@ class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 0, 
-      bacon: 0,
-      cheese: 0,
-      meat: 0
+      bacon: 2,
+      cheese: 1,
+      meat: 2,
     }
   }
 
   render() {
     return (
       <Aux>
-        < Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls />
       </Aux>
     );
   }
