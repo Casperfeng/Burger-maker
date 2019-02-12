60b62fbe12e41666aa3fb78fe7cc29bd9cff598a
Casperfeng <casp.feng@gmail.com>
2019-02-12 11:19:02 +0100

    Layout now passes props to conditionally render sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,13 +6,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 //Aux is used to avoid using a div tag
 class Layout extends Component {
+
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false}); 
+  }
+
   render(){
     return (
     <Aux>
       <Toolbar />
-      <SideDrawer />
+      <SideDrawer 
+      closed={this.sideDrawerClosedHandler}
+      show={this.state.showSideDrawer} />
       <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
       </main>
     </Aux>
     );
