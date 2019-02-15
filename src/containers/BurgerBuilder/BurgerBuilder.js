b489e7dbac51efcfd587e2b5782f41e140432473
Casperfeng <casp.feng@gmail.com>
2019-02-15 14:43:58 +0100

    modal now updates

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,6 +85,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You continue!');
+        this.setState( {loading: true} );
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -100,12 +101,12 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'fastest'
 
         }
-        axios.post('/orders.json', order).then(response => 
-            console.log(response)
-        ).catch(error => 
-            console.log(error)
-        );
-
+        axios.post('/orders.json', order).then(response => {
+            this.setState( {loading: false, purchasing: false} );
+        }
+        ).catch(error => { 
+            this.setState( {loading: false, purchasing: false} );
+        });
     }
 
     render () {
